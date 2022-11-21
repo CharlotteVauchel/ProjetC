a09e31282662c6a06f9fb2782df214b2bf0462df
CharlotteVauchel <vauchelcharlotte@gmail.com>
2022-11-21 11:31:15 +0100

    TODO6 et tests

--- Partie1/main.c
+++ Partie1/main.c
@@ -1,5 +1,13 @@
 #include "./lib/io.h"
 
+//TODO6
+//c generic function 
+#define printG(val) _Generic((val), \
+                    char: printChar, \
+                    char *: printString, \
+                    default: printInteger  \
+              )(val)
+
 int main()
 {
     printChar('a');
@@ -9,5 +17,19 @@ int main()
     printInteger(85247923125976314);
     printChar('\n');
     newPrintf("L'entier vaut : %d, le charactère est : %c, et le string est : %s", 111, 'A', "hello");
+    printChar('\n');
+
+    //TODO6 tests
+    char c = 'c';
+    int val = 4;
+    int integ = 14;
+    char * str = "bonjour";
+    printG(c);
+    printChar('\n');
+    printG(val);
+    printChar('\n');
+    printG(14);
+    printChar('\n');
+    printG(str);
     printChar('\n');
 }
